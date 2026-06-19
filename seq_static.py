5dfb0b00f7ad13b184b7d3794e7306a8f8b2871e
Mads.ino <96733004+MadsHerniksen@users.noreply.github.com>
2026-06-19 04:23:42 +0200

    Add files via upload

--- seq_static.py
+++ seq_static.py
@@ -2,3 +2,12 @@ class Seq:
     __init__(self):
         self.seqIndex = 0
         self.seqmode = None
+        self.clock = 0
+        self.times = [6,12,24,48,96]
+        self.time = 12
+        
+    def getClock(self):
+        self.clock = self.clock + 1
+        if self.clock == self.time:
+            self.seqIndex = self.seqIndex + 1
+            self.clock = 0
